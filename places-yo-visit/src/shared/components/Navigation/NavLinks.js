44730abc21ce33feb5ec5ee1d27d6129a4e1a9ef
Nitish Choudhary <nitishchy007@gmail.com>
2023-04-25 21:15:59 +0530

    places to go frontend done

--- places-yo-visit/src/shared/components/Navigation/NavLinks.js
+++ places-yo-visit/src/shared/components/Navigation/NavLinks.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../../context/auth-context";
 import { NavLink } from "react-router-dom";
+
 import "./NavLinks.css";
 
 const NavLinks = (props) => {
+    const auth = useContext(AuthContext);
     return (
         <ul className="nav-links">
             <NavLink to="/" exact>
                 ALL USERS
             </NavLink>
-            <NavLink to="/:userId/places" exact>
-                MY PLACES
-            </NavLink>
-            <NavLink to="/places/new">ADD PLACE</NavLink>
-            <NavLink to="/auth">AUTHENTICATE</NavLink>
+            {auth.isLoggedIn && (
+                <NavLink to="/:userId/places" exact>
+                    MY PLACES
+                </NavLink>
+            )}
+            {auth.isLoggedIn && <NavLink to="/places/new">ADD PLACE</NavLink>}
+            {!auth.isLoggedIn && <NavLink to="/auth">AUTHENTICATE</NavLink>}
+            {auth.isLoggedIn && (
+                <li>
+                    <button onClick={auth.logout}>LOGOUT</button>
+                </li>
+            )}
         </ul>
     );
 };
--- places-yo-visit/src/users/pages/Auth.js
+++ places-yo-visit/src/users/pages/Auth.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import Card from "../../shared/components/UIElements/Card";
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
 
 import { useForm } from "../../shared/hooks/form-hook";
+import { AuthContext } from "../../shared/context/auth-context";
 import {
     VALIDATOR_EMAIL,
     VALIDATOR_MINLENGTH,
+    VALIDATOR_REQUIRE,
 } from "../../shared/util/validators";
 
 import "./Auth.css";
 
 function Auth() {
-    const [formState, inputHandler] = useForm(
+    const auth = useContext(AuthContext);
+    const [isLoginMode, setIsLoginModeMode] = useState(true);
+    const [formState, inputHandler, setFormData] = useForm(
         {
             email: {
                 value: "",
@@ -29,14 +33,42 @@ function Auth() {
     const authSubmitHandler = (event) => {
         event.preventDefault();
         console.log(formState.inputs);
+        auth.login();
+    };
+
+    const isLoginModeModeHandler = () => {
+        if (!isLoginMode)
+            setFormData(
+                { ...formState.inputs, name: undefined },
+                formState.inputs.email.isValid &&
+                    formState.inputs.password.isValid
+            );
+        else {
+            setFormData(
+                { ...formState.inputs, name: { value: "", isValid: false } },
+                false
+            );
+        }
+        setIsLoginModeMode((prev) => !prev);
     };
 
     return (
         <div>
             <Card className="authentication">
-                <h2>Login Required</h2>
+                <h2>{!isLoginMode ? "Sign Up" : "Login Required"}</h2>
                 <hr />
                 <form>
+                    {!isLoginMode && (
+                        <Input
+                            element="input"
+                            type="text"
+                            id="name"
+                            label="Name"
+                            validators={[VALIDATOR_REQUIRE()]}
+                            errorText="Please enter your name"
+                            onInput={inputHandler}
+                        />
+                    )}
                     <Input
                         element="input"
                         // value={formState.email.value}
@@ -61,9 +93,12 @@ function Auth() {
                         onClick={authSubmitHandler}
                         disabled={!formState.isValid}
                     >
-                        LOGIN
+                        {!isLoginMode ? "SIGNUP" : "LOGIN"}
                     </Button>
                 </form>
+                <Button inverse onClick={isLoginModeModeHandler}>
+                    SWITCH TO SIGNUP
+                </Button>
             </Card>
         </div>
     );
